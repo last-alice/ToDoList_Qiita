d452f009a9fdafc884fef78e4517d150d2df9e09
last_alice <iolite.kinseiseki18@gmail.com>
2021-08-04 12:06:24 +0900

    ヘッダーコンポーネント実装

--- vue-laravel-spa/resources/js/app.js
+++ vue-laravel-spa/resources/js/app.js
@@ -18,8 +18,10 @@ window.Vue = require('vue').default;
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import HeaderComponent from "./components/HeaderComponent";
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('header-component', HeaderComponent)
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
